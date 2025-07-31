38cfd900ccb54fd4de28b02e18b2d13c7ed04810
KaizenNotDev <bugilcok1306@gmail.com>
2025-08-01 01:10:41 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+API_ID = 123456  # Ganti dengan API ID kamu
+API_HASH = "your_api_hash"  # Ganti dengan API HASH kamu
+BOT_TOKEN = "your_bot_token"  # Ganti dengan token bot kamu
+OWNER_ID = 123456789  # ID Telegram kamu (admin bot)
